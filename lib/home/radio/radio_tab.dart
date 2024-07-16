bae0dd6c51095b3bb9f1f732fa524617f01bafa3
EL-mohands <amrmarei87@gmail.com>
2024-07-15 17:47:22 -0700

    Finish UI and logic sebha tab and finish UI radio tab.

--- lib/home/radio/radio_tab.dart
+++ lib/home/radio/radio_tab.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
+    return Column(
+      children: [
+        Expanded(flex: 2, child: Image.asset('assets/image/radio_logo.png')),
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                'اذاعه القرأن الكريم',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/image/icon_prev.png'),
+                  Image.asset('assets/image/icon_play.png'),
+                  Image.asset('assets/image/icon_next.png'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/home/sebha/sebha_tab.dart
+++ lib/home/sebha/sebha_tab.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/app_colors.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
+  @override
+  _SebhaTabState createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int counter = 0;
+  double rotationAngle = 0.0;
+  List<String> phrases = [
+    'سبحان الله',
+    'الحمد لله',
+    'لا إله إلا الله',
+    'الله أكبر'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: incrementCounter,
+          child: Transform.rotate(
+            angle: rotationAngle,
+            child: Image.asset('assets/image/sebha_logo.png'),
+          ),
+        ),
+        Text(
+          'عدد التسبيحات',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.symmetric(horizontal: 160.0, vertical: 10),
+          decoration: BoxDecoration(
+            color: AppColors.primaryLightColor,
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Text(
+            '$counter',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24.0,
+              color: AppColors.blackColor,
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(10.0),
+          margin: EdgeInsets.symmetric(horizontal: 110.0, vertical: 60),
+          decoration: BoxDecoration(
+            color: AppColors.primaryLightColor,
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Text(
+            getCurrentPhrase(),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24.0,
+              color: AppColors.blackColor,
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: resetTasbeeh,
+          child: Text('إعادة تعيين'),
+          style: ElevatedButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.titleLarge,
+            primary: AppColors.primaryLightColor,
+            onPrimary: AppColors.blackColor,
+          ),
+        ),
+      ],
     );
+  }
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+      rotationAngle += 15.0;
+    });
+  }
+
+  String getCurrentPhrase() {
+    int phraseIndex = (counter ~/ 33) % phrases.length;
+    return phrases[phraseIndex];
+  }
+
+  void resetTasbeeh() {
+    setState(() {
+      counter = 0;
+      rotationAngle = 0.0;
+    });
   }
 }
